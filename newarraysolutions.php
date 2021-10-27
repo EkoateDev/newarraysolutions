57c2b5081f73fda982dc2ae00dd95abc1f589940
EkoateDev <ekoate@globaledge-software.com>
2021-10-27 10:40:51 +0400

    up till question 6

---
+++ newarraysolutions.php
@@ -0,0 +1,125 @@
+<?php
+// $color = [
+//     'white',
+//     'green',
+//     'red',
+//     'blue',
+//     'black'
+// ];
+
+// echo 'The memory of that scene for me is like a frame of film forever frozen at that moment: the ' . $color[2] . ' carpet, 
+// the ' . $color[1] . ' lawn, the ' . $color[0] . ' house, the leaden sky. The new president and his first lady. - Richard M. Nixon';
+
+// Question 2 
+
+// $color = [
+//     'white',
+//     'green',
+//     'red'
+// ];
+
+// echo '<em> Output :</em>';
+// echo '<br>';
+
+// echo $color[0] . ',' . $color[1] . ',' . $color[2] . ',';
+
+// echo '<br>';
+// echo '<ul> 
+// <li> ' . $color[1] . '</li>
+// <li> ' . $color[2] . '</li> 
+// <li> ' . $color[0] . '</li> 
+// </ul>';
+
+
+// Question 3
+
+// $ceu = [
+//     'Italy' => 'Rome',
+//     'Luxembourg' => 'Luxembourg',
+//     'Belgium' => 'Brussels',
+//     'Denmark' => 'Copenhagen',
+//     'Finland' => 'Helsinki',
+//     'France' => 'Paris',
+//     'Slovakia' => 'Bratislava',
+//     'Slovenia' => 'Ljubljana',
+//     'Germany' => 'Berlin',
+//     'Greece' => 'Athens',
+//     'Ireland' => 'Dublin',
+//     'Netherlands' => 'Amsterdam',
+//     'Portugal' => 'Lisbon',
+//     'Spain' => 'Madrid',
+//     'Sweden' => 'Stockholm',
+//     'United Kingdom' => 'London',
+//     'Cyprus' => 'Nicosia',
+//     'Lithuania' => 'Vilnius',
+//     'Czech Republic' => 'Prague',
+//     'Estonia' => 'Tallin',
+//     'Hungary' => 'Budapest',
+//     'Latvia' => 'Riga',
+//     'Malta' => 'Valetta',
+//     'Austria' => 'Vienna',
+//     'Poland' => 'Warsaw'
+// ];
+
+// asort($ceu);
+
+// echo '<em> Sample Output :</em>';
+// echo '<br>';
+
+// foreach ($ceu as $country => $capital) {
+//     echo 'The capital of ' . $country . ' is ' . $capital;
+//     echo '<br>';
+// }
+
+
+//  Question 4
+
+// $x = [
+//     1,
+//     2,
+//     3,
+//     4,
+//     5
+// ];
+
+// echo '<em> Sample Output :</em>';
+// echo '<br>';
+
+// $newXValue = $x;
+
+// var_dump($newXValue);
+
+// echo '<br>';
+
+// unset($x[3]);
+
+// $newArrayVulues =  array_values($x);
+
+// var_dump($newArrayVulues);
+
+
+// Question 5 
+
+// $color = [
+//     4 => 'white',
+//     6 => 'green',
+//     11 => 'red'
+// ];
+
+// echo 'This is where the pointer is';
+// echo '<br>';
+
+// echo current($color);
+
+// echo '<br>';
+// echo 'Moving the Array forward';
+
+// echo '<br>';
+// echo next($color);
+
+// echo '<br>';
+// echo 'Getting the first element';
+
+// echo '<br>';
+// $newColors = $color;
+// echo '<em> Expected Result : </em>' . reset($newColors);
