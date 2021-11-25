f0a6fc36ff9d91f4a4354beace1f3e94b6d58752
EkoateDev <ekoate@globaledge-software.com>
2021-11-25 16:25:02 +0400

    worked on classes again.

--- OOP/classes.php
+++ OOP/classes.php
@@ -1,35 +1,6 @@
 <?php
-
-// class is a self-contained independent collection of variables and fuctions which work together 
-
-// objects are instances of a class, they inherit properties and behaviours from a class
-//  but each object has a different value for properties 
-
-// in a class variables are called properties and functions are called methods 
-
-
-class Fruit
-{
-    // properties 
-    public $name;
-    public $color;
-
-    // Methods 
-    function setName($name)
-    {
-        $this->name = $name;
-    }
-    function getName($name)
-    {
-        return $this->$name;
-    }
-}
-
-//  creating objects from a class using new keyword
-
 class Car
 {
-
     public $name;
     public $color;
     public $engine;
@@ -74,15 +45,15 @@ echo '<br>';
 echo 'the engine built for the car is ' . $petronas->getEngine();
 
 // changing the value of a property outside a method and class 
-class Veggies
+class ShoeBrands
 {
     public $type;
 }
 
-$potatoes = new Veggies();
-$potatoes->type = 'Tomatoes';
+$sportsSneakers = new ShoeBrands;
+$sportsSneakers->type = 'Puma Future';
 
-echo $potatoes->type;
+echo $sportsSneakers->type;
 
 // using instance of to check if an object belongs to a class
 
